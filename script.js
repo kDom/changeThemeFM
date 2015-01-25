35fe5f6152ef8e2b6f069788d676d0a486cc3228
K. <vif.kirito@gmail.com>
2015-01-25 19:57:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 var theme = {
   blue = {
     name : "Blue Skin",
-    style : "/blue-skin.css"
+    style : "/blue-skin.css",
+    jquery : "/blue-skin.js"
   }
   red = {
     name : "Red Skin",
-    style : "/red-skin.css"
+    style : "/red-skin.css",
+    jquery : "/red-skin.js"
   }
   black = {
     name : "Black Skin",
-    style : "/black-skin.css"
+    style : "/black-skin.css",
+    jquery : "/black-skin.js"
   }
 }
