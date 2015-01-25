42c7d28c8ad66fa17eb9c65c8cb03349be926b9d
K. <vif.kirito@gmail.com>
2015-01-25 19:56:11 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+var theme = {
+  blue = {
+    name : "Blue Skin",
+    style : "/blue-skin.css"
+  }
+  red = {
+    name : "Red Skin",
+    style : "/red-skin.css"
+  }
+  black = {
+    name : "Black Skin",
+    style : "/black-skin.css"
+  }
+}
